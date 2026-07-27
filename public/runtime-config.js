fd7d69e9545991faf162dba14a8a8458848bfcb1
syconsulting2026-glitch <syconsulting2026@gmail.com>
2026-07-27 14:23:33 +0900

    api url 수정

--- public/runtime-config.js
+++ public/runtime-config.js
@@ -1,5 +1,5 @@
 // 정적 배포 후 이 파일의 API_URL을 실제 Node.js 백엔드 주소로 바꾸세요.
 // 예: window.__ZEROV_CONFIG__ = { API_URL: "https://api.zerov.co.kr" };
 window.__ZEROV_CONFIG__ = {
-  API_URL: "http://localhost:4000"
+  API_URL: "https://palegoldenrod-gull-895963.hostingersite.com/"
 };
